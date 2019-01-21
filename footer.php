b05b8d3f1c128f9d7bbc4f163834dc4e55c264b1
florin bucataru <bucataruflorin89@gmail.com>
2019-01-21 10:01:18 +0000

    heroku test 1

---
+++ footer.php
@@ -0,0 +1,6 @@
+ <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white"> <?php echo $footer_copyright_name; ?> <?php echo date('Y');?></p>
+      </div>
+      <!-- /.container -->
+    </footer>
---
+++ variables.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$site_title = "dog grooming inc.";
+$footer_copyright_name="dogGrooming.com";
+$sub_title="styling dogs since 2004";
+
+
+
+
+
+?>
